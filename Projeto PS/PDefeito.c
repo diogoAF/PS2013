56e7d504eab65138bc3ca16a812b8b4a7623f5fb
mashiro11 <thiagoaas1990@gmail.com>
2013-12-02 06:08:34 -0800

    :D

--- Projeto PS/PDefeito.c
+++ Projeto PS/PDefeito.c
@@ -1,1 +1,226 @@
+#define SPDEFEITO_SERV
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "SPDefeito.h"
+#include "PacoteDeStructs.h"
+
+ int inserirDefeito(FILE *arquivo, TipoDefeito *defeito){
+
+    char espacoVago[Tamanho + 1];//+1: espaço para armazenar o caracter NULL
+    espacoVago[Tamanho] = NULL;
+    fread(espacoVago, sizeof(char), Tamanho, arquivo);
+    long int posicaoVaga = strtol(espacoVago, NULL, HEXA);
+
+    /*ajusta as strings para ficarem com formato exato para serem escritas em arquivo*/
+    ajustaString(defeito->codigo, VET_CODIGO);
+    ajustaString(defeito->descricao, VET_DESCRICAO);
+    ajustaString(defeito->codigoProduto, VET_CODIGO);
+
+    if(posicaoVaga == FALSE){
+        //os "-1" são para evitar escrever o caractere "NULL" no arquivo
+        fseek(arquivo, 0, SEEK_END);
+
+        fwrite(defeito->codigo, sizeof(char), VET_CODIGO - 1, arquivo);
+        fprintf(arquivo, " ");
+
+        fwrite(defeito->codigoProduto, sizeof(char), VET_CODIGO - 1, arquivo);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%d", defeito->estado);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%0*d", DGTS_VOTO, defeito->votos);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%0*d", DGTS_DIA, defeito->dataAbertura.dia);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_MES, defeito->dataAbertura.mes);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_ANO, defeito->dataAbertura.ano);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%0*d", DGTS_DIA, defeito->dataEncerramento.dia);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_MES, defeito->dataEncerramento.mes);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_ANO, defeito->dataEncerramento.ano);
+        fprintf(arquivo, " ");
+
+        fwrite(defeito->descricao, sizeof(char), VET_DESCRICAO - 1, arquivo);
+        fprintf(arquivo, "\n");
+
+    }else{
+        //printf("posicaoVaga: %d\n", posicaoVaga);
+        fseek(arquivo, posicaoVaga + 1, SEEK_SET);//+1:ignora o * indicador de espaço vago
+        fread(espacoVago, sizeof(char), Tamanho, arquivo);//leitura do posicionamento do proximo espaço vago
+
+        fseek(arquivo, posicaoVaga, SEEK_SET);//volta o carro para o inicio do registro
+        fwrite(defeito->codigo, sizeof(char), VET_CODIGO - 1, arquivo);
+        fprintf(arquivo, " ");
+
+        fwrite(defeito->codigoProduto, sizeof(char), VET_CODIGO - 1, arquivo);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%d", defeito->estado);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%0*d",DGTS_VOTO, defeito->votos);
+        fprintf(arquivo, " ");
+
+
+        fprintf(arquivo, "%0*d", DGTS_DIA, defeito->dataAbertura.dia);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_MES, defeito->dataAbertura.mes);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_ANO, defeito->dataAbertura.ano);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%0*d", DGTS_DIA, defeito->dataEncerramento.dia);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_MES, defeito->dataEncerramento.mes);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_ANO, defeito->dataEncerramento.ano);
+        fprintf(arquivo, " ");
+
+        fwrite(defeito->descricao, sizeof(char), VET_DESCRICAO - 1, arquivo);
+        fprintf(arquivo, "\n");
+
+        fseek(arquivo, 0, SEEK_SET);
+        fwrite(espacoVago, sizeof(char), Tamanho, arquivo);
+    }
+ }
+
+ int deletarDefeito(FILE * arquivo, TipoDefeito * registro){
+    //verifica o topo da pilha de espacos vazios
+    char disponivel[Tamanho + 1];
+    fseek(arquivo, SEEK_SET, SEEK_SET);
+    fread(disponivel, sizeof(char), Tamanho, arquivo);
+    disponivel[ Tamanho ] = NULL;
+    fseek(arquivo, Tamanho + 2, SEEK_SET);
+    //+1: ignora o "\n" existente ao final da linha
+
+    //busca o registro no arquivo
+    char buffer[VET_CODIGO];//chave primaria
+    char lixo[TAM_TOTAL_REG_DEFEITO - VET_CODIGO];
+    long int posicao;
+    do{
+        posicao = ftell(arquivo);
+        fread(buffer, sizeof(char), VET_CODIGO - 1, arquivo);
+        buffer[VET_CODIGO - 1] = NULL;
+        fread(lixo, sizeof(char), TAM_TOTAL_REG_DEFEITO - VET_CODIGO-1, arquivo);
+        lixo[TAM_TOTAL_REG_DEFEITO - VET_CODIGO-1] = NULL;
+    }while( strcmp(buffer, registro->codigo) );
+
+    fseek(arquivo, 0, SEEK_SET);
+    fprintf(arquivo, "%0*x",Tamanho, posicao);
+    //a posição é salva em hexadecimal para aumentar o alcance do indice num numero menor de caracteres
+    fseek(arquivo, posicao, SEEK_SET);
+    fprintf(arquivo, "*%s", disponivel);
+ }
+
+ int ajustaString(char *buffer, int tamanho){
+    buffer[ tamanho - 1 ] = NULL;
+    int cont = strlen(buffer);
+    if(cont < tamanho){
+        int i;
+        for(i = 0; i < ( tamanho - cont - 1); i++){
+            buffer[cont + i] = ' ';
+        }
+    }
+
+ }
+
+int existeDefeito(FILE *arquivo, char *chavePrimaria, long int *posicao){
+    ajustaString(chavePrimaria, VET_CODIGO);
+    char buffer[VET_CODIGO];
+    char lixo[TAM_TOTAL_REG_DEFEITO - VET_CODIGO + 1];
+    fseek(arquivo, Tamanho + 2, SEEK_SET);
+    do{
+        fread(buffer, sizeof(char), VET_CODIGO - 1, arquivo);
+        buffer[VET_CODIGO - 1] = NULL;
+        if( !strcmp(buffer, chavePrimaria) ){
+            *posicao = ftell(arquivo) - VET_CODIGO + 1;
+            return TRUE;
+        }
+        fread(lixo, sizeof(char), TAM_TOTAL_REG_DEFEITO - VET_CODIGO-1, arquivo);
+        lixo[TAM_TOTAL_REG_DEFEITO - VET_CODIGO-1] = NULL;
+    }while( !feof(arquivo) );
+    *posicao = NULL;
+    return FALSE;
+}
+
+int editarDefeito(FILE *arquivo, TipoDefeito *defeito, long int posicao){
+    fseek(arquivo, posicao, SEEK_SET);
+    fwrite(defeito->codigo, sizeof(char), VET_CODIGO - 1, arquivo);
+        fprintf(arquivo, " ");
+
+        fwrite(defeito->codigoProduto, sizeof(char), VET_CODIGO - 1, arquivo);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%d", defeito->estado);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%0*d", DGTS_VOTO, defeito->votos);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%0*d", DGTS_DIA, defeito->dataAbertura.dia);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_MES, defeito->dataAbertura.mes);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_ANO, defeito->dataAbertura.ano);
+        fprintf(arquivo, " ");
+
+        fprintf(arquivo, "%0*d", DGTS_DIA, defeito->dataEncerramento.dia);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_MES, defeito->dataEncerramento.mes);
+        fprintf(arquivo, "-");
+
+        fprintf(arquivo, "%0*d", DGTS_ANO, defeito->dataEncerramento.ano);
+        fprintf(arquivo, " ");
+
+        fwrite(defeito->descricao, sizeof(char), VET_DESCRICAO - 1, arquivo);
+        fprintf(arquivo, "\n");
+}
+
+int editarDefeitoCodigo(FILE *arquivo, char *codigo, long int posicao){
+    fseek(arquivo, posicao, SEEK_SET);
+    ajustaString(codigo, VET_CODIGO);
+    fwrite(codigo, sizeof(char), VET_CODIGO-1, arquivo);
+}
+
+//int editarDefeitoDescricao(FILE *arquivo, char *descricao, long int posicao){
+//    fseek(arquivo, posicao + VET_CODIGO, SEEK_SET);
+//    fwrite(descricao, sizeof(char), VET_DESCRICAO-1, arquivo);
+//}
+//
+//int editarDefeitoCodigoProduto(FILE *arquivo, char *codigoProduto, long int posicao){
+//    fseek(arquivo, posicao + VET_CODIGO + VET_DESCRICAO, SEEK_SET);
+//    fwrite(codigoProduto, sizeof(char), VET_CODIGO-1, arquivo);
+//}
+//
+int editarDefeitoVoto(FILE *arquivo, long int posicao){
+    fseek(arquivo, posicao + 2*(VET_CODIGO-1) + DGTS_ESTADO + 3, SEEK_SET);
+    char votos[DGTS_VOTO+1];
+    votos[DGTS_VOTO] = NULL;
+    fread(votos, sizeof(char), DGTS_VOTO, arquivo);
+    int numVotos = strtol(votos, NULL, DECI);
+    if(numVotos < 999){
+            numVotos++;
+            fseek(arquivo, posicao + 2*(VET_CODIGO-1) + DGTS_ESTADO + 3, SEEK_SET);
+            fprintf(arquivo, "%0*d", DGTS_VOTO, numVotos);
+    }
+}
--- Projeto PS/SPDefeito.h
+++ Projeto PS/SPDefeito.h
@@ -1,53 +1,73 @@
-//#ifndef SPDEFEITO_H_INCLUDED
-//#define SPDEFEITO_H_INCLUDED
-//
-////cadastrar()
-////remover()
-////editar()
-////existência()
-//
-///** \brief Função verifica a existência de um registro
-// *
-// * \param
-// * \param
-// * \return
-// *
-// */
-//int existencia();
-//
-///** \brief Função cadastra um novo usuario
-// *
-// * \param
-// * \param
-// * \return
-// *
-// */
-// int cadastrar(FILE*, ListDesenvolvedor*);
-//
-// /** \brief Função responsavvel por deletar um registro
-//  *
-//  * \param
-//  * \param
-//  * \return
-//  *
-//  */
-// int deletar();
-//
-//
-// /** \brief Função responsavel por editar um registro dado
-//  *
-//  * \param
-//  * \param
-//  * \return
-//  *
-//  */
-// int editar();
-//
-//
-//
-//
-//
-//
-//
-//
-//#endif // SPDEFEITO_H_INCLUDED
+#ifndef SPDEFEITO_H_INCLUDED
+#define SPDEFEITO_H_INCLUDED
+
+#ifdef SPDEFEITO_SERV
+#define EXT extern
+#else EXT
+#endif
+
+/** \brief Função de cadastro de defeito
+ *
+ * \param Ponteiro ao arquivo que guarda os registros dos defeitos.
+ * \param Estrutura de dados que armazena as informações do defeito em memória.
+ * \return Codigo de Sucesso/Erro.
+ *
+ */
+EXT int inserirDefeito(FILE *, TipoDefeito *);
+
+
+/** \brief Função responsável por deletar um registro de defeito.
+ *
+ * \param Ponteiro ao arquivo que armazena as informações dos defeitos.
+ * \param Estrutura de dados que armazena as informações do defeito em memória.//Pode ser trocada pelo codigo
+ * \return Codigo de Sucesso/Erro
+ *
+ */
+EXT int deletarDefeito(FILE * arquivo, TipoDefeito * registro);
+
+/** \brief Busca um registro no arquivo
+ *
+ * \param Ponteiro ao arquivo que armazena as informações dos defeitos.
+ * \param String da chave primária de busca no arquivo
+ * \param Endereço a um long int que poderá receber a posição do registro no arquivo, caso encontrado
+ * \return Código de Sucesso/Fracasso
+ *
+ */
+EXT int existeDefeito(FILE *arquivo, char *chavePrimaria, long int *posicao);
+
+
+
+/** \brief Edita todos os campos do registro de um defeito
+ *
+ * \param Ponteiro ao arquivo que armazena as informações dos defeitos.
+ * \param Estrutura de dados que armazena as informações do defeito em memória.
+ * \param Long int com a posição do registro no arquivo
+ * \return Código de Sucesso/Erro
+ *
+ */
+EXT int editarDefeito(FILE *arquivo, TipoDefeito *defeito, long int posicao);
+
+
+/** \brief Edita campo código do registro de um defeito
+ *
+ * \param Ponteiro ao arquivo que armazena as informações dos defeitos.
+ * \param String que armazena o novo codigo
+ * \param Long int com a posição do registro no arquivo
+ * \return Código de Sucesso/Erro
+ *
+ */
+EXT int editarDefeitoCodigo(FILE *arquivo, char *codigo, long int posicao);
+
+
+
+/** \brief Edita campo votos do registro de um defeito
+ *
+ * \param Ponteiro ao arquivo que armazena as informações dos defeitos.
+ * \param Long int com a posição do registro no arquivo
+ * \return Código de Sucesso/Erro
+ *
+ */
+EXT int editarDefeitoVoto(FILE *arquivo, long int posicao);
+
+
+#endif // SPDEFEITO_H_INCLUDED
